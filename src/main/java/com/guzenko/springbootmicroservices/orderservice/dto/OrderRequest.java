d671ffef9a332493f019f0182f3ab1225fa2f2db
guzen <guzenko.dmitriy@gmail.com>
2025-02-14 07:28:33 +0200

    InventoryClient

---
+++ src/main/java/com/guzenko/springbootmicroservices/orderservice/dto/OrderRequest.java
@@ -0,0 +1,10 @@
+package com.guzenko.springbootmicroservices.orderservice.dto;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id
+                            , String orderNumber
+                            , String skuCode
+                            , BigDecimal price
+                            , Integer quantity) {
+}
---
+++ src/main/java/com/guzenko/springbootmicroservices/orderservice/service/OrderService.java
@@ -0,0 +1,37 @@
+package com.guzenko.springbootmicroservices.orderservice.service;
+
+import com.guzenko.springbootmicroservices.orderservice.client.InventoryClient;
+import com.guzenko.springbootmicroservices.orderservice.dto.OrderRequest;
+import com.guzenko.springbootmicroservices.orderservice.model.Order;
+import com.guzenko.springbootmicroservices.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }
+        else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
+
+
+    }
+}
